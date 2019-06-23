e1a7e909bf11210ea246770966ddff86a7c5b12e
wangct <wang975840786@qq.com>
2019-06-24 00:24:03 +0800

    暂时存储

--- test/index.js
+++ test/index.js
@@ -2,10 +2,9 @@
  * Created by wangct on 2018/12/23.
  */
 
-const fs  = require('fs');
-const path = require('path');
 const util = require('../lib');
 
+console.log(util.getLocalIp());
 
 util.getOuterNetIp().then(d => {
   console.log(d);
