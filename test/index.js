5ff8402583304349e8fee9c66139436fe147cffe
wangct <wang975840786@qq.com>
2019-07-07 14:07:40 +0800

    no message

--- test/index.js
+++ test/index.js
@@ -2,10 +2,25 @@
  * Created by wangct on 2018/12/23.
  */
 
-const util = require('../lib');
+const request = require('request');
+const fs = require('fs');
+const ws = fs.createWriteStream('test/a.wav');
 
-console.log(util.getLocalIp());
 
-util.getOuterNetIp().then(d => {
-  console.log(d);
-});
+request('https://tsn.baidu.com/text2audio',{
+  method:'post',
+  header:{
+
+  },
+  form:{
+    tex:'测测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试试',
+    tok:'24.e558edafde38b2b8fd01c51f77691461.2592000.1565018547.282335-16727634',
+    cuid:'12345postman',
+    ctp:'1',
+    lan:'zh',
+    spd:'5',
+    vol:'5',
+    per:'1',
+    aue:'6'
+  }
+}).pipe(ws);
