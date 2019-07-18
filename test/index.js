462209c557c33b725e79de164418af2acdd0e065
wangct <wang975840786@qq.com>
2019-07-18 23:31:31 +0800

    no message

--- test/index.js
+++ test/index.js
@@ -4,4 +4,15 @@
 
 const util = require('../lib');
 
-console.log(util);
+util.eachFile('lib',{
+  execItem(filepath){
+    return new Promise(cb => {
+      setTimeout(() => {
+        cb();
+      },500);
+      console.log(filepath);
+    })
+  }
+}).then(list => {
+  console.log(list);
+})
