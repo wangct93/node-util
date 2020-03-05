fbb07f617372314fb21a5b3dc89c145ff1874864
wangct <wang975840786@qq.com>
2020-03-05 23:30:53 +0800

    '提升'

---
+++ unitTest/options.js
@@ -0,0 +1,16 @@
+const {resolve} = require("../lib");
+
+
+module.exports = {
+  // eachFile:[
+  //   {
+  //     i:[resolve('test')],
+  //     o:['1','2','3'],
+  //     oFunc:(item) => {
+  //       item.then(d => {
+  //         console.log(d);
+  //       })
+  //     }
+  //   },
+  // ],
+};
