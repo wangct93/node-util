eca2e6de13811f995b0b21bfdae7c07629cd9219
wangct <wang975840786@qq.com>
2020-07-21 23:43:41 +0800

    '新版本'

--- test/index.js
+++ test/index.js
@@ -1,11 +1,8 @@
-/**
- * Created by wangct on 2018/12/23.
- */
-
-const util = require('../lib');
+const {deleteFile} = require("../lib/file");
+const {eachFile} = require("../lib/file");
 
 
+const {Server} = require('../lib');
 
-util.watch('test',() => {
-  console.log(12311);
-})
+
+deleteFile('temp/ad');
